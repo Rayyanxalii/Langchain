5462a878311e68f51c85207dcb34ada45a5cff24
Rayyanxalii <alirayyan986@gmail.com>
2026-07-15 23:11:20 +0500

    More addition

---
+++ ChatModels/chat_anthropic.py
@@ -0,0 +1,10 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatAnthropic(model = 'claude-2')
+
+result = llm.invoke('What is the capital of Pakistan?')
+
+print(result.content)
